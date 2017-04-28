7908b3147f4a85ff6efa053d1e07b1a48382340b
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-04-28 15:52:18 -0700

    Got stoplights to toggle on and off with click events

--- index.js
+++ index.js
@@ -2,4 +2,30 @@
   'use strict';
 
   // YOUR CODE HERE
+
+  let buttonDivs = document.querySelectorAll('div .button');
+  let bulbDivs = document.querySelectorAll('div .bulb');
+
+  buttonDivs.forEach( (div) => {
+    div.addEventListener('click', transform);
+  })
+
+  function transform(event) {
+    let eventText = event.target.innerText.toLowerCase();
+
+    for (let bulb in bulbDivs) {
+      if (bulbDivs[bulb].id.includes(eventText)) {
+        bulbDivs[bulb].classList.toggle(eventText);
+      }
+
+    }
+
+
+}
+
+
+
+
+
+
 })();
