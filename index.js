66062661e5ee29c4e7ac108e8c1d9b4dd7ca5f18
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-04-28 17:41:01 -0700

    Made separate callbacks for click events, mouseover, and mouseout, so that the console is able to log different messages for each event

--- index.js
+++ index.js
@@ -6,23 +6,59 @@
   let buttonDivs = document.querySelectorAll('div .button');
   let bulbDivs = document.querySelectorAll('div .bulb');
 
-  buttonDivs.forEach( (div) => {
-    div.addEventListener('click', transform);
+  buttonDivs.forEach((div) => {
+    div.addEventListener('click', bulbTOGGLE);
   })
 
-  function transform(event) {
+  buttonDivs.forEach((div) => {
+    div.addEventListener('mouseover', bulbON);
+  })
+
+
+  buttonDivs.forEach((div) => {
+    div.addEventListener('mouseout', bulbOFF);
+  })
+
+  function bulbTOGGLE(event) { // This one logs nothing when called
+
     let eventText = event.target.innerText.toLowerCase();
 
     for (let bulb in bulbDivs) {
-      if (bulbDivs[bulb].id.includes(eventText)) {
+      let idText = bulbDivs[bulb].id;
+      if (idText.includes(eventText)) {
         bulbDivs[bulb].classList.toggle(eventText);
       }
-
     }
 
+  }
 
-}
+  function bulbON(event) { // This one logs that mouseover happened
 
+    let eventText = event.target.innerText.toLowerCase();
+
+    for (let bulb in bulbDivs) {
+      let idText = bulbDivs[bulb].id;
+      if (idText.includes(eventText)) {
+        bulbDivs[bulb].classList.toggle(eventText);
+        console.log(`Entered ${eventText} button`);
+      }
+    }
+
+  }
+
+  function bulbOFF(event) { // This one logs that mouseout happened
+
+    let eventText = event.target.innerText.toLowerCase();
+
+    for (let bulb in bulbDivs) {
+      let idText = bulbDivs[bulb].id;
+      if (idText.includes(eventText)) {
+        bulbDivs[bulb].classList.toggle(eventText);
+        console.log(`Left ${eventText} button`);
+      }
+    }
+
+  }
 
 
 
